eaae09f3b05f3afdba05fa0c15010d06d78ab878
smnadalin <smnadalin@gmail.com>
2023-06-07 22:31:00 +1000

    Added meta information

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 import Nav from "./Nav";
+import logo from "./Images/Logo.svg";
 
 function App() {
   return (
     <>
-      <head></head>
+      <head>
+        <meta name="description" content="Little Lemon greek restaurant" />
+        <meta name="og:title" content="Little Lemon" />
+        <meta name="og:description" content="Little Lemon greek restaurant" />
+        <meta name="og:image" content={logo} />
+      </head>
       <body>
         <Nav />
         <Header />
